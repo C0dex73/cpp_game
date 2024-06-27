e2c238cf8b64fec86ae585659fcab5168d2ffebf
C0dex73 <c0d3x73@gmail.com>
2024-06-27 17:22:01 +0200

    first window

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
-int main()
-{
-    // Initialize GLFW
-    if (!glfwInit())
+int main(){
+    if( !glfwInit() )
     {
+        fprintf( stderr, "Failed to initialize GLFW.\r\n" );
         return -1;
     }
 
-    // Create a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Game Engine", NULL, NULL);
-    if (!window)
-    {
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "Test1", NULL, NULL);
+    if( !window ) {
+        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible.\r\n");
         glfwTerminate();
         return -1;
     }
 
-    // Make the window's context current
     glfwMakeContextCurrent(window);
-
-    // Main game loop
-    while (!glfwWindowShouldClose(window))
-    {
-        // Input handling
-        glfwPollEvents();
-
-        // Rendering code goes here
-
-        // Swap buffers
-        glfwSwapBuffers(window);
+    if (glewInit() != 0) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        return -1;
     }
 
-    // Clean up
-    glfwTerminate();
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, 1);
+
+    do{
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }while( glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0 );
 
     return 0;
 }
