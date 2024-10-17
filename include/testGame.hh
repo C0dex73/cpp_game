57007261cf48b71e96e686bfa7b40f1b730fb442
C0dex73 <c0d3x73@gmail.com>
2024-10-17 03:00:41 +0200

    user can add rectangles --NOT STABLE : RANDOM SEGFLT--

--- include/testGame.hh
+++ include/testGame.hh
@@ -3,6 +3,7 @@
 
 #include "gameTemplate.hh"
 #include "displayable.hh"
+#include <vector>
 
 namespace cdxg {
 
@@ -14,7 +15,15 @@ namespace cdxg {
         void Update() override;
         void Render() override;
         private:
-        Displayable<VertexRGB> rectangle;
+        std::vector<Displayable<VertexRGB>> rectangles;
+        int state = -1;
+        void addRectangle();
+        void processInput();
+        std::vector<VertexRGB> vertices;
+        std::vector<unsigned int> indices;
+        int oldAddButtonState = 0;
+        int oldSubButtonState = 0;
+        int oldSpaceButtonState = 0;
     }; // class TestGame
 
 } // namespace cdxg
--- src/testGame.cc
+++ src/testGame.cc
@@ -13,7 +13,14 @@
 namespace cdxg{
 
     TestGame::TestGame(int initialWidth, int initialHeight, std::string initialTitle):
-        GameTemplate(initialWidth, initialHeight, initialTitle)
+        GameTemplate(initialWidth, initialHeight, initialTitle),
+        vertices({
+            VertexRGB(-0.2, -0.2, 1.0, 0.0, 0.0),
+            VertexRGB(0.2, 0.2, 0.0, 1.0, 0.0),
+            VertexRGB(-0.2, 0.2, 0.0, 0.0, 1.0),
+            VertexRGB(0.2, -0.2, 1.0, 1.0, 1.0)
+        }),
+        indices({0, 1, 2, 0, 1, 3})
         {}
 
     TestGame::~TestGame(){}
@@ -21,43 +28,58 @@ namespace cdxg{
     void TestGame::Initialize(){
         // initialize Uinput
         glfwSetInputMode(DisplayManager::mpsWindow, GLFW_STICKY_KEYS, 0);
-
-        static std::vector<VertexRGB> v = {
-            VertexRGB(-0.2, -0.2, 1.0, 0.0, 0.0),
-            VertexRGB(0.2, 0.2, 0.0, 1.0, 0.0),
-            VertexRGB(-0.2, 0.2, 0.0, 0.0, 1.0),
-            VertexRGB(0.2, -0.2, 1.0, 1.0, 1.0)
-        };
-        rectangle.setVertices(v, false);
-        static std::vector<unsigned int> i = {
-            0, 1, 2, 0, 1, 3
-        };
-        rectangle.setVerticesIndices(i, false);
-        static Shader s = Shader(&RawShaders::rgb);
-        rectangle.setShader(&s);
-        rectangle.load();
-
         //initialize GameTime
         GameTime::msDeltaTimeSec = 0.0f;
         GameTime::msTotalTimeSec = glfwGetTime();
     } // void Initialize()
 
-    void TestGame::Update(){
+    void TestGame::addRectangle(){
+        rectangles.push_back(Displayable<VertexRGB>());
+        ++state;
+        rectangles[state].setVerticesIndices(indices, false);
+        rectangles[state].setVertices(vertices, false);
+        rectangles[state].setShader(Shader::defaultShader());
+        rectangles[state].load();
+    }
+
+    void TestGame::processInput(){
+        if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_KP_ADD) == GLFW_PRESS && oldAddButtonState == GLFW_RELEASE){
+            addRectangle();
+        }
+        if(state == -1) { return; }
+
         if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_LEFT) == GLFW_PRESS){
-            rectangle.move(-0.005, 0.0);
+            rectangles[state].move(-0.005, 0.0);
         }
         if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_RIGHT) == GLFW_PRESS){
-            rectangle.move(0.005, 0.0);
+            rectangles[state].move(0.005, 0.0);
         }
         if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_UP) == GLFW_PRESS){
-            rectangle.move(0.0, 0.005);
+            rectangles[state].move(0.0, 0.005);
         }
         if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_DOWN) == GLFW_PRESS){
-            rectangle.move(0.0, -0.005);
+            rectangles[state].move(0.0, -0.005);
         }
+        
+        if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_KP_SUBTRACT) == GLFW_PRESS && oldSubButtonState == GLFW_RELEASE){
+            rectangles.erase(rectangles.begin() + state);
+            if(rectangles.size() == 0) { state = -1; }
+        }
+        if(glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_SPACE) == GLFW_PRESS && oldSpaceButtonState == GLFW_RELEASE){
+            ++state;
+        }
+        if(state >= rectangles.size()) { state = 0; }
+    }
+
+    void TestGame::Update(){
         //update GameTime
         GameTime::msDeltaTimeSec = glfwGetTime() - GameTime::msTotalTimeSec;
         GameTime::msTotalTimeSec = glfwGetTime();
+
+        processInput();
+        oldAddButtonState = glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_KP_ADD);
+        oldSubButtonState = glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_KP_SUBTRACT);
+        oldSpaceButtonState = glfwGetKey(DisplayManager::mpsWindow, GLFW_KEY_SPACE);
     } // void Update()
 
     void TestGame::Render(){
@@ -65,7 +87,9 @@ namespace cdxg{
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        rectangle.draw();
+        for(int i = 0; i < rectangles.size(); ++i){
+            rectangles[i].draw();
+        }
         
         // display the current frame and init the next one
         glfwSwapBuffers(DisplayManager::mpsWindow);
